da5b18119c9c6f02540afbe2aad60f655f4ac8e0
Augusto <augustoperaltamontero@gmail.com>
2020-09-04 20:55:14 +0000

    First

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO PRINCESS PATRICK
         </p>
         <a
           className="App-link"
